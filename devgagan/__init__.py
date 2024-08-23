55c95f84ed9874e795e778f264f26dfb34f146ce
kaybe gamdu <alice548@veilvault.online>
2024-08-23 18:00:26 +0000

    __init__.py edited online with Bitbucket

--- devgagan/__init__.py
+++ devgagan/__init__.py
@@ -23,7 +23,7 @@ app = Client(
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
-    workers=10,
+    workers=27,
     sleep_threshold=20,
     max_concurrent_transmissions=1000
 )
